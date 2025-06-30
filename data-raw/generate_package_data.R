2e7afd85c49d2e8f0df386c359512f4b4e038c52
Miquel De Cáceres <miquelcaceres@gmail.com>
2025-06-30 18:21:30 +0200

    dataset documentation

--- data-raw/generate_package_data.R
+++ data-raw/generate_package_data.R
@@ -1,7 +1,14 @@
 indicator_definition <-as.data.frame(readxl::read_xlsx("data-raw/forestindicators_input.xlsx",
-                                                       sheet="indicators"), stringsAsFactors=FALSE)
-usethis::use_data(indicator_definition, overwrite = TRUE)
+                                                       sheet="indicators"), stringsAsFactors=FALSE) |>
+  dplyr::mutate(stand_dynamic_input = as.logical(stand_dynamic_input),
+                plant_static_input = as.logical(plant_static_input),
+                plant_dynamic_input = as.logical(plant_dynamic_input))
 
 variable_definition <-as.data.frame(readxl::read_xlsx("data-raw/forestindicators_input.xlsx",
                                                       sheet="variables"), stringsAsFactors=FALSE)
+
+# TODO: Check that variables cited in indicator definition are actually defined in variable definition
+
+
+usethis::use_data(indicator_definition, overwrite = TRUE)
 usethis::use_data(variable_definition, overwrite = TRUE)
