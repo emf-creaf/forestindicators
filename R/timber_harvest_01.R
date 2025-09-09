444dea387f2e8e66feaf6bad817025b3832b9ea2
nuaquilue <nuaquilue@gmail.com>
2025-09-09 16:38:04 +0200

    timber_harvest with ifn_volume

--- R/timber_harvest_01.R
+++ R/timber_harvest_01.R
@@ -1,3 +1,5 @@
+## All outputs will be at the plot-level
+
 .timber_harvest_01 <- function(stand_static_input,
                                stand_dynamic_input = NULL,
                                plant_static_input = NULL,
@@ -10,10 +12,16 @@
   if(!inherits(a, "numeric")) cli::cli_abort("'a' should be a numeric value") 
   
   ## Filter plant_dynamic_input by state = "cut"
-  tree_cut = plant_dynamic_input |> filter(state == "cut")  
+  plant_input = plant_dynamic_input |> dplyr::filter(state == "live")  
   
+  ##
+  # stand_static_input$Province = 8  # do it in the console!
+  plant_input$vol = timber_volume_function(stand_static_input, plant_input)
   
+  ## Summarize
+  df <- plant_input |> dplyr::group_by(id_stand, date) |> dplyr::summarise(timber_harvest_01 = sum(vol))
+ 
   ## Return the output data frame
-  res = df |> select(id_stand, date, timber_harvest_01)
+  res = df |> dplyr::select(id_stand, date, timber_harvest_01)
   return(res)
 }
